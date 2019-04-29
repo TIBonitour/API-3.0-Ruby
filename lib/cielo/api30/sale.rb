f7af901aa0f5ba4fd951607024ad0e0410ad82ee
Rosemberg <rosembergdesantana@gmail.com>
2019-04-29 12:50:55 -0300

    Adicionado parse para json e validacao de chaves para venda

--- lib/cielo/api30/sale.rb
+++ lib/cielo/api30/sale.rb
@@ -19,11 +19,11 @@ module Cielo
 
       def self.from_json(data)
         return if data.nil?
-
+        data = JSON.parse(data)
         sale = new(data["MerchantOrderId"])
-        sale.customer = Customer.from_json(data["Customer"])
-        sale.payment = Payment.from_json(data["Payment"])
-        sale.fraud_analysis = FraudAnalysis.from_json(data["FraudAnalysis"])
+        sale.customer = Customer.from_json(JSON.generate(data["Customer"])) unless data["Customer"].nil?
+        sale.payment = Payment.from_json(JSON.generate(data["Payment"])) unless data["Payment"].nil?
+        sale.fraud_analysis = FraudAnalysis.from_json(JSON.generate(data["FraudAnalysis"])) unless data["FraudAnalysis"].nil?
         sale
       end
 
