d3715f53418fa9cfd91c6eb956ab7d2d47cba0c9
Rosemberg <rosembergdesantana@gmail.com>
2019-04-29 14:32:28 -0300

    Renomeado arquivo e alterado path de testes

---
+++ spec/api30/payment_spec.rb
@@ -0,0 +1,39 @@
+require "cielo/api30/payment"
+
+describe Cielo::API30::Payment do
+  subject { described_class.new(123, 2) }
+  let(:json) { "{\"Installments\":2,\"Amount\":123}" }
+
+  describe "#initialize" do
+    it "should set the attributes based on the given code" do
+      expect(subject.amount).to eql(123)
+      expect(subject.installments).to eql(2)
+    end
+  end
+
+  describe "#to_json" do
+    it "should return a json" do
+      expect(subject.to_json).to eql(json)
+    end
+  end
+
+  describe "#from_json" do
+    it "should return a object" do
+      expect(Cielo::API30::Payment.from_json(json).class).to eql(subject.class)
+    end
+  end
+
+  describe "#success?" do
+    it "should return a object" do
+      subject.status = 1
+      expect(subject.success?).to be_truthy
+    end
+  end
+
+  describe "#as_json" do
+    it "should have the same value" do
+      expect(subject.as_json.has_key?(:Amount)).to be_truthy
+      expect(subject.as_json.has_key?(:Installments)).to be_truthy
+    end
+  end
+end
--- spec/payment/status_spec.rb
+++ spec/payment/status_spec.rb
@@ -1,11 +1,18 @@
 require "cielo/api30/payment/status"
 
 describe Cielo::API30::Payment::Status do
-  let(:success) { described_class.success?(0) }
+  let(:zero) { described_class.success?(0) }
+  let(:um) { described_class.success?(1) }
 
-  describe "#client" do
-    it "should return the same object" do
-      expect(success).to be_falsey
+  describe "#success?" do
+    it "should return false" do
+      expect(zero).to be_falsey
+    end
+  end
+
+  describe "#success?" do
+    it "should return true" do
+      expect(um).to be_truthy
     end
   end
 end
